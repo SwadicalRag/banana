5b442e5316f3e60e566787604312c25c733beae2
Swad <SwadicalRag@users.noreply.github.com>
2016-01-26 18:06:57 +1100

    Super Namespace Banana

--- lua/banana/banana.lua
+++ lua/banana/banana.lua
@@ -5,9 +5,13 @@ local tostring = tostring
 local getmetatable = getmetatable
 local setmetatable = setmetatable
 
-banana = {}
+local banana = {}
+local BANANA_NAMESPACE,BANANA_CLASS = 0,1
 
-banana.Classes = {}
+banana.Protected = {
+    Extends = true,
+    GetName = true
+}
 
 local function copy(source,lookup)
     lookup = lookup or {}
@@ -16,40 +20,96 @@ local function copy(source,lookup)
     local target = {}
     lookup[source] = target
 
-    setmetatable(target,copy(getmetatable(source)) or {})
-
     for key,value in pairs(source) do
         target[copy(key,lookup)] = copy(value,lookup)
     end
 
+    setmetatable(target,copy(getmetatable(source)) or {})
+
     return target
 end
 
-function banana.Define(name)
-    local class = setmetatable({},{
-        __tostring = function(self)
-            return "Base Class "..name
-        end
+function banana.__new(enum)
+    return setmetatable({},{
+        __banana = enum
     })
+end
 
-    banana.Classes[name] = class
+banana.RootNamespace = banana.__new(BANANA_NAMESPACE)
+
+function banana.resolveNamespace(name)
+    assert(string.match(name,"[A-Za-z0-9:]+"),"Class names must be alphanumeric!")
+    local namespace = banana.RootNamespace
+
+    for chunk,sep in string.gmatch(name,"([A-Za-z0-9]+)(:+)") do
+        if sep and sep ~= "::" then error("Bad namespace separation convention "..sep) end
+        namespace[chunk] = namespace[chunk] or banana.__new(BANANA_NAMESPACE)
+        if not banana.is(namespace[chunk],BANANA_NAMESPACE) then error("Namespace "..name.." passes through a class!") end
+        namespace = namespace[chunk]
+    end
+
+    return namespace,string.match(name,"([A-Za-z0-9]+)$")
+end
+
+function banana.resolveNamespaceEx(name)
+    assert(string.match(name,"[A-Za-z0-9:]+"),"Class names must be alphanumeric!")
+    local namespace = banana.RootNamespace
+
+    for chunk,sep in string.gmatch(name,"([A-Za-z0-9]+)(:*)") do
+        if sep and sep ~= "::" and sep ~= "" then break end
+        namespace[chunk] = namespace[chunk] or banana.__new(BANANA_NAMESPACE)
+        if not banana.is(namespace[chunk],BANANA_NAMESPACE) then error("Namespace "..name.." passes through a class!") end
+        namespace = namespace[chunk]
+    end
+
+    return namespace
+end
+
+function banana.Define(fullname)
+    local classMeta = {}
+    classMeta.__banana = BANANA_CLASS
+
+    function classMeta:__tostring()
+        return "Base Class "..fullname
+    end
+    classMeta.__extends = {}
+    function classMeta:__index(k)
+        for _,parentClass in ipairs(classMeta.__extends) do
+            if parentClass[k] then return parentClass[k] end
+        end
+    end
+
+    local space,name = banana.resolveNamespace(name)
+
+    local class = setmetatable({},classMeta)
+    space[name] = class
+
+    function class:GetName()
+        return fullname
+    end
 
     function class:Extends(extends)
-        local from = copy(banana.Classes[extends] or {})
-
-        for k,v in pairs(from) do
-            self[k] = v
+        local extNamespace,name = banana.resolveNamespace(extends)
+        if extNamespace[name] then
+            classMeta.__extends[#classMeta.__extends+1] = extNamespace[name]
+        else
+            error("Unknown class "..extends)
         end
 
         return class
+    end
+
+    function classMeta:__newindex(k,v)
+        assert(banana.Protected[k],"Cannot modify protected member '"..k.."'")
+        rawset(self,k,v)
     end
 
     return class
 end
 
 function banana.New(name)
-    assert(banana.Classes[name],"Class "..tostring(name).." does not exist!")
-    local instance = copy(banana.Classes[name])
+    assert(space[name],"Class "..tostring(name).." does not exist!")
+    local instance = copy(space[name])
 
     setmetatable(instance,{
         __tostring = function(self)
@@ -68,10 +128,29 @@ function banana.New(name)
     return instance
 end
 
-function banana.Peel()
-    local peel = copy(banana)
-    peel.Classes = {}
-    return peel
+function banana.PrintNamespace(namespace,depth,prepend,ntype)
+    depth = depth or -1
+    prepend = prepend or ""
+    local space = (type(namespace) == "string") and banana.resolveNamespaceEx(namespace) or namespace
+    local indent = ("    "):rep(depth)
+
+    ntype = ntype or "Namespace"
+
+    print(indent.."== "..ntype.." "..(((type(namespace) == "string") and namespace) or prepend:sub(1,-3)).." ==")
+    depth = depth + 1
+
+    indent = ("    "):rep(depth)
+    for k,v in pairs(space) do
+        if type(v) == "table" then
+            if banana.__is(BANANA_CLASS) then
+                banana.PrintNamespace(v,depth + 1,k.."::","Class")
+            elseif banana.__is(BANANA_NAMESPACE) then
+                banana.PrintNamespace(v,depth + 1,k.."::","Namespace")
+            end
+        elseif type(v) == "function" then
+            print(indent..prepend..k.." -> Method")
+        end
+    end
 end
 
 banana.Clone = copy
